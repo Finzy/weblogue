c2aa21b1c64604421be529044bb6c86d646a058f
Perry van Zetten <15840@ma-web.nl>
2015-03-04 13:55:58 +0100

    thumbnails

--- weblogue/front-page.php
+++ weblogue/front-page.php
@@ -5,9 +5,14 @@
     $args = array( 'numberposts' => '6' );
     $recent_posts = wp_get_recent_posts( $args );
     $gallery = get_post_gallery_images( $args );
+    $thumb = the_post_thumbnail($args);
+
     foreach( $recent_posts as $recent ){
-        echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
-    }
+        if ( has_post_thumbnail() ) {
+            echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"]. $thumb.'</a> </li> ';
+        }
+ }
 ?>
+
 </ul>
 </main>
