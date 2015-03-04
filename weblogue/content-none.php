b52692ec2c32d29ff076bb1a586d59ba1b2e8c21
Farhat Abdad <farhat.a@live.com>
2015-03-04 13:17:32 +0100

    search bar is in

--- weblogue/content-none.php
+++ weblogue/content-none.php
@@ -21,8 +21,6 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'weblogue' ); ?></p>
-			<?php get_search_form(); ?>
-
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'weblogue' ); ?></p>
--- weblogue/content-search.php
+++ weblogue/content-search.php
@@ -18,7 +18,6 @@
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
